c56302b7eaf1e1517805e37bcce4c5deb5134b20
Evgenia Katsada <evgkatsada@gmail.com>
2020-07-14 16:05:56 +0300

    Experiment with xml configuration for beans

--- src/main/java/demo/springframework/jokesapp/config/ChuckConfiguration.java
+++ src/main/java/demo/springframework/jokesapp/config/ChuckConfiguration.java
@@ -4,10 +4,14 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/**
+ * This class provides bean configuration through @Configuration.
+ * For now everything is commented, as the xml bean configuration is used
+ */
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
